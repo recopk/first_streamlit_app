4ee4342dd1a7ce584d34bce7336b68e20bd6e005
recopk <146513604+recopk@users.noreply.github.com>
2023-10-01 09:13:50 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@ import streamlit
 streamlit.title('My parents new healthy menu)'
 streamlit.title('Omega 3 & Oatmeal)'
 streamlit.title('hard boiled free range egg')
-streamlit.title('best healthy drinks made with yougurt)'
+streamlit.title('best healthy drinks made with yougurt')
