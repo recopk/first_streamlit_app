9f41838f6bab200b63f5f1ddfbbe6accb7709e76
recopk <146513604+recopk@users.noreply.github.com>
2023-10-01 09:40:15 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,6 @@ streamlit.text('🥑 Omega 3 & Oatmeal')
 streamlit.text('🐔 hard boiled free range egg')
 streamlit.text('🥗 best healthy drinks made with yougurt')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
